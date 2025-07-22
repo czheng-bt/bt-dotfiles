5e56223eae98f09a2eb9a957b01697e6d5d2b720
Chongyi Zheng <czheng@belvederetrading.com>
2025-07-22 14:00:05 -0500

    Install oh-my-zsh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if ! command -v zsh >/dev/null 2>&1
+then
+  sudo yun install zsh -y
+fi
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
