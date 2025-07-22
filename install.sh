bdf553aa93d2c4c886ecf19971c7c53321f39b14
Chongyi Zheng <czheng@belvederetrading.com>
2025-07-22 14:06:07 -0500

    Fix yum command

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 if ! command -v zsh >/dev/null 2>&1
 then
-  sudo yun install zsh -y
+  sudo yum install zsh -y
 fi
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
